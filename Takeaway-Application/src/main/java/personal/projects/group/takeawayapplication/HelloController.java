58b339cc61c0d099289a531331fb72be8188e21a
Alex Downman <alexdownman@gmail.com>
2025-03-11 11:17:28 +0000

    added extra files added comments to try and help understanding

--- Takeaway-Application/src/main/java/personal/projects/group/takeawayapplication/HelloController.java
+++ Takeaway-Application/src/main/java/personal/projects/group/takeawayapplication/HelloController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    //Similar to Javascript DOM
+
+    //@FXML adds it to part of the FXML file
     @FXML
     private Label welcomeText;
 
