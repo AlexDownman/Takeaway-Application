9f336adcc20792c6b35af551db1116f073e8f402
Shayon <156447440+IceSpice64@users.noreply.github.com>
2025-03-13 01:03:29 +0000

    Database Table Addition and changes

--- Takeaway-Application/src/main/java/DatabaseManager/DatabaseHandler.java
+++ Takeaway-Application/src/main/java/DatabaseManager/DatabaseHandler.java
@@ -47,7 +47,9 @@ public class DatabaseHandler {
 
             // Print rows
             while (rs.next()) {
-                System.out.printf("%s %s %s\n", rs.getString(1), rs.getString(2), rs.getString(3)); // Adjust based on your table structure
+                System.out.printf("%s %s %s %s\n", rs.getString(1), rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4)); // Adjust based on your table structure
             }
 
             rs.close();
