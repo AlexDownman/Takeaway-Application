5edaa06dd03c63bb3bff37fbf6be59436d0090bf
alex downman <alexdownman@gmail.com>
2025-03-17 00:06:49 +0000

    Added Setters for Customer objects

--- Takeaway-Application/src/main/java/Tablehandlers/Customer.java
+++ Takeaway-Application/src/main/java/Tablehandlers/Customer.java
@@ -19,11 +19,46 @@ public class Customer {
     }
 
     // Getters
-    public String getName() { return name; }
-    public String getPhone() { return phone; }
-    public String getEmail() { return email; }
-    public String getAddress() { return address; }
-    public String getPostcode() { return postcode; }
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
 
     // Validate the customer object
     public boolean isValid() {
