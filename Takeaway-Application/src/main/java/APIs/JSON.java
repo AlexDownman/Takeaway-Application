0b5e3d54506eef92a5af4ee9e7710f4cb8b4b10f
alex downman <alexdownman@gmail.com>
2025-04-10 19:27:20 +0100

    Added API.java to handle fetch/read Added JSON to handle JSON related functions

---
+++ Takeaway-Application/src/main/java/APIs/JSON.java
@@ -0,0 +1,26 @@
+package APIs;
+
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JSON {
+    public static Map<String, Object> jsonToMap(JSONObject json) {
+        Map<String, Object> map = new HashMap<>();
+        for (Object keyObj : json.keySet()) {
+            String key = (String) keyObj;
+            Object value = json.get(key);
+
+            // If the value is a nested JSONObject, convert it recursively
+            if (value instanceof JSONObject) {
+                value = jsonToMap((JSONObject) value);
+            }
+
+            // Optionally handle JSONArray too if needed
+
+            map.put(key, value);
+        }
+        return map;
+    }
+}
